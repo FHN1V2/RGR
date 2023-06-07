1d998dc8dbcd5230f80d1ac45d9c78cd5bbf6c61
FHN1V2 <135702884+FHN1V2@users.noreply.github.com>
2023-06-07 17:36:21 +0700

    Delete VigenereCipher.h

--- VigenereCipher.h
+++
@@ -1,9 +0,0 @@
-#ifndef VIGENERECIPHER_H
-#define VIGENERECIPHER_H
-
-#include <string>
-
-void encryptVigenere(std::string& message, const std::string& key);
-void decryptVigenere(std::string& message, const std::string& key);
-
-#endif  // VIGENERECIPHER_H
